8fbb37f9647d6f28401b91ed327a054f7f2b1a15
Sieng <143239359+nguyendinhsieng1983@users.noreply.github.com>
2024-01-26 11:55:09 +0200

    name

--- Sieng_tasks/ext1.php
+++ Sieng_tasks/ext1.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercise 1: Getting Started with PHP - Andy</title>
+    <title>Exercise 1: Getting Started with PHP - Sieng</title>
 </head>
 <body>
     <h3>3.1 Write PHP code to output the following message:
     Hello world! My name is "David"
     </h3>
         <?php
-            echo "Hello world! My name is \"Andy\""; //Remember to use \ to exit the double quote ""
+            echo "Hello world! My name is \"Sieng\""; //Remember to use \ to exit the double quote ""
         ?>
     <h3>3.2 Create a PHP variable named $title and assign it the value "PHP is interesting." 
         Then, use this variable as the content within an (heading 4) element.
