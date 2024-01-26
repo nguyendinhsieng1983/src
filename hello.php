813d72f2325c8e8487f92bcceea9541dc0c0781c
Sieng <143239359+nguyendinhsieng1983@users.noreply.github.com>
2024-01-26 10:59:06 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+
+
+<?php
+// this is printing hello Mr Andy
+/*mutiple
+line comments
+
+
+*/
+echo "<h1>Hello Mr Andy</h1>";
+?>
+
+
+    
+</body>
+</html>
