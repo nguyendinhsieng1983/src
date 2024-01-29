f8bc5d0285b0870562bca50d33768988db2b3958
Sieng <143239359+nguyendinhsieng1983@users.noreply.github.com>
2024-01-29 19:41:05 +0700

    Add files via upload

---
+++ Sieng_tasks/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="header.css">
+    <link rel="stylesheet" type="text/css" href="styling-phuoc.css">
+    <script src="https://kit.fontawesome.com/192e3b220b.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<div class="main">
+        <div class="header">        
+            <div class="container-lg pt-3">
+                <div class="row"> 
+                    <nav class="menu">
+                        <div class="logo">
+                            <a href="/Portfolio_Project/"><img class="header-logo" alt="G6 Portfolio Logo" src="image/favicon_io/header-favicon-32x32.png"></a>
+                        </div>
+                        <input type="checkbox" id="check">
+                        <label for="check" class="checkbtn">
+                            <i class="fa-solid fa-bars"></i>
+                        </label>
+                        <ul role="menubar" class="nav-list">
+                            <hr class="divider">
+                              <li role="menuitem" class="has-children">
+                                  <a href="ex1.php">Exercise 1 </a>
+                              </li>
+                              <li role="menuitem" class="has-children">
+                                  <a href="ex2.php">Exersices 2</a>
+                              </li>
+                              <li role="menuitem" class="has-children">
+                                  <a href="/Portfolio_Project/team.html">Exercise 3</a>
+                              </li>
+                              <li role="menuitem" class="has-children">
+                                  <a href="/Portfolio_Project/contact.html">Exercise 4</a>
+                              </li>
+                            <hr class="divider">
+                        </ul>
+                    </nav>
+                    
+                </div>
+        </div>
+</div>
