944ffb587fbeb1f1d583a0b614e650c34d931259
Sieng <143239359+nguyendinhsieng1983@users.noreply.github.com>
2024-02-02 16:41:09 +0700

    Add files via upload

--- Sieng_tasks/ex4.php
+++ Sieng_tasks/ex4.php
@@ -1,35 +1,73 @@
 <?php
-$title = "Exercise 4: Control flow and loops";
-include 'lect2/header.php'; ?>
+$title="Exercise 4:  Control flow and loops";
+include 'header.php'; ?>
 
-<h1> Write a PHP script to get inputs (age and name) from the user and based on their age,   
-     decide if he/she is eligible for voting. (18 or more than 18 years is eligible for voting, use form to get user input).</h1>
-  
-   <from name"form1" method= "post"
+<div class="container"> 
+    <h4>4.2. If-Else: Write a PHP script to get inputs (age and name) from the user and based on their age, 
+decide if he/she is eligible for voting. (18 or more than 18 years is eligible for voting)
+    </h4>
+    <form method="post" action="CheckAge.php">
+        Enter your name: <input type="text" name="name"><br><br>
+        Enter your age: <input type="number" name="age"><br><br>
+        <input type="submit" value="Submit" class="btn btn-outline-success">
+    </form>
+    <!-- End of 4.2-->
+    
+    <br>
+    <h4>4.3. Switch Case: Write a PHP script that gets the current month and prints one of the following responses,
+        depending on whether it's August or not:
+        It's August, so it's still holiday.
+        Not August, this is Month-name so I don't have any holidays
+            <i><p>Hint: You can use date(F) function to get the current month name. Check Date function from the php manual page.</p></i>
+    </h4>
+        <?php
+        // Get the current month name using the date() function
+        $currentMonth = date('F');
 
-<from method= "post" action = <?php                       
-  Name: <input type="text" name="nimi"> required <br><br>
-  Email: <input type="email" name="email"> required <br><br>
-  DOB: <input type="date" name="dob"> <br><br>
-  Select color: <input type="color" name="color"> <br><br>
-  <input type="submit" value="Submit">
-</from>
+        // Use a switch statement to display the appropriate message
+        switch ($currentMonth) {
+            case 'August':
+                echo "It's August, so it's still holiday!";
+                break;
+            default:
+                echo "Not August, this is $currentMonth so I don't have any holidays.";
+        }
+        ?>
+    <!-- End of 4.3-->
 
+    <br>
+    <h4>4.4. For Loop: Write a PHP script that will print the multiplication table of a number (n)</h4>
+        <form method="post" action="ForLoop.php">
+            Enter a number: <input type="number" name="number"><br><br>
+            <input type="submit" value="Generate Table" class="btn btn-outline-success">
+        </form>
+        
+    <!-- End of 4.4-->
+    
+    <br>
+    <h4>4.5. While Loop: Write a PHP script that will print all the numbers from 1 to n </h4>
+         <form method="post" action="WhileLoop.php">
+            Enter the 'n' number: <input type="number" name="number"><br><br>
+            <input type="submit" value="Submit" class="btn btn-outline-success">
+        </form>
+    <!-- End of 4.5-->
 
+    <br>
+    <h4>4.6. Foreach Loop: Write a PHP script that will print all the elements of an array. </h4>
+        <?php
+        echo "My array including: "."<br>";
+        // Define an array with some elements
+        $myArray = array("apple", "banana", "orange", "grape", "cherry");
 
+        // Use a foreach loop to iterate through the array and print each element
+        
+        foreach ($myArray as $fruit) {
+            echo $fruit . "<br>"; // Each element will be printed on a new line
+        }
+        ?>
+    <!-- End of 4.6-->
+
+</div> <!-- End of container center for body content-->
 <?php
-$str1 ="Hello"; &&  $str2 = "World";
-echo "Hello World"; 
+include 'footer.php';
 ?>
-
-
-<?php
-//processing form in the same page
-
-// check for a post request
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-  $name = $_POST["nimi"];
-  $email = $_POST["email"];
-}
-
-<?php include 'lect2/footer.php'; ?>
